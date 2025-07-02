9cc3e12b4916f07cba3dd201280e3f0cc8a66a36
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-07-02 22:21:37 +0530

    updated all methods

--- include/imu.h
+++ include/imu.h
@@ -1,7 +1,9 @@
 #ifndef IMU_H
 #define IMU_H
 
-void imu_init();
-float get_heading();    // returns angle in degrees
+void imu_init();            // initialize and calibrate the imu
+void imu_update();          // call this regularly (every 5-10ms)
+float imu_get_heading();        // returns angle in degrees
+float imu_get_raw_angle();  // (optional) for direct use if normalization added
 
 #endif
