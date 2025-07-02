4caa9fcd549e580be522642c97f520188250bb34
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-07-02 16:52:15 +0530

    added boolean methods for wall checking

--- include/ir_sensor.h
+++ include/ir_sensor.h
@@ -2,8 +2,13 @@
 #define IR_SENSOR_H
 
 void ir_sensor_init();
+
 int read_front_ir();
 int read_left_ir();
 int read_right_ir();
+
+bool is_wall_front();
+bool is_Wall_left();
+bool is_wall_right();
 
 #endif
