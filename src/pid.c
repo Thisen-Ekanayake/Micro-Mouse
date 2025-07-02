1457a3ad767a03db3cc5e969806c791cded93793
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-07-02 20:04:00 +0530

    add pid_reset() method

--- src/pid.c
+++ src/pid.c
@@ -1,9 +1,15 @@
 #include "pid.h"
 
-float pid_compute(PID *pid, float setpoint, float current) {
-    float error = setpoint - current;
+void pid_reset(PID *pid) {
+    pid->integral = 0;
+    pid->prev_error = 0;
+}
+
+float pid_compute(PID *pid, float setpoint, float measurement) {
+    float error = setpoint - measurement;
     pid->integral += error;
     float derivative = error - pid->prev_error;
     pid->prev_error = error;
-    return pid->kp * error + pid->ki * pid->integral + pid->kd * derivative;
+
+    return (pid->kp * error) + (pid->ki * pid->integral) + (pid->kd * derivative);
 }
