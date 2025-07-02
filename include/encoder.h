d70b25c42199db777b3b1bf267dd681df01e33d3
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-07-02 20:25:51 +0530

    added get_avg_ticks() method

--- include/encoder.h
+++ include/encoder.h
@@ -1,9 +1,10 @@
 #ifndef ENCODER_H
 #define ENCODER_H
 
-void init_encoder();
+void encoder_init();
 void reset_ticks();
 int get_left_ticks();
 int get_right_ticks();
+int get_avg_ticks();
 
 #endif
