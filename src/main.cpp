9eeb0e6734ec99dae99156d7d4fd9fd42eefaf3b
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-07-02 20:03:25 +0530

    added testing block

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "button.h"
 #include "maze.h"
 #include "floodfill.h"
+#include "motion.h"
 
 void setup() {
     Serial.begin(115200);
@@ -21,4 +22,27 @@ void setup() {
 
 void loop() {
     flood_fill();
+
+    /*
+
+    testing
+    move_forward_cm(18);   // move 1 cell
+    rotate_90_left();
+
+    How to Calibrate it (Once You Have Hardware)
+    Do this:
+
+    Move the micromouse exactly 18 cm forward using move_forward_cm(18);
+    Print the number of encoder ticks:
+
+    Serial.println(get_left_ticks());
+
+    Compute:
+    TICKS_PER_CM = (left_ticks + right_ticks) / 2.0 / 18.0
+    
+    Then update the config.h value accordingly.
+
+    */
+
+
 }
