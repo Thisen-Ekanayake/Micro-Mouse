8df343e853f20e6858f03cd5c11cd8c6e5f0b979
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-07-06 09:58:38 +0530

    added motor_set_speed_ramp() method

--- include/motor.h
+++ include/motor.h
@@ -5,5 +5,5 @@ void motor_init();
 void motor_set_speed(int left_pwm, int right_pwm);
 void motor_speed();
 void motor_stop();
-
+void motor_set_speed_ramp(int left_target, int right_target, int ramp_step, int delay_ms);
 #endif
