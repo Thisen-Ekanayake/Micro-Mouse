a2d27eb49d94610a4585251bd42ac5a66415a291
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-07-06 10:35:45 +0530

    added comments

--- include/button.h
+++ include/button.h
@@ -1,7 +1,7 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
-void button_init();
-int button_pressed();
+void button_init();     // call in setup()
+int button_pressed();   // returns 1 if pressed, 0 otherwise
 
 #endif
