c3ff15116fb19fd3d367029a1d480152a4bbb828
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-07-06 09:44:33 +0530

    added comments

--- include/ir_sensor.h
+++ include/ir_sensor.h
@@ -1,12 +1,15 @@
 #ifndef IR_SENSOR_H
 #define IR_SENSOR_H
 
+// initialize all ir sensor pins
 void ir_sensor_init();
 
+// returns raw ADC values from sensors
 int read_ir_front();
 int read_ir_left();
 int read_ir_right();
 
+// returns true if wall is detected (based on thresholds in config.h)
 bool is_wall_front();
 bool is_wall_left();
 bool is_wall_right();
