38ff74c75065c65cf777d628ce07441fa72bb7b5
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-07-08 10:42:04 +0530

    modified logic

--- include/maze.h
+++ include/maze.h
@@ -1,16 +1,26 @@
 #ifndef MAZE_H
 #define MAZE_H
+
 #include <stdint.h>
 
-typedef struct {
-    uint8_t walls;
-    uint8_t visited;
-    uint8_t distance;
-} Cell;
+// direction enum
+typedef enum {
+    NORTH   = 0,
+    EAST    = 1,
+    SOUTH   = 2,
+    WEST    = 3
+} Direction;
 
-void init_maze();
-Cell* get_cell(uint8_t x, uint8_t y);
-void set_wall(uint8_t x, uint8_t y, char dir);
+// wall bitmask : 4 bits per cell
+#define WALL_NORTH (1 << NORTH)
+#define WALL_EAST (1 << EAST)
+#define WALL_SOUTH (1 << SOUTH)
+#define WALL_WEST (1 << WEST)
+
+void maze_init();
+void set_wall(int x, int y, Direction dir);
+int has_Wall(int x, int y, Direction dir);
+int is_valid_cell(int x, int y);
 void print_maze();
 
 #endif
