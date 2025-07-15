57ca4249bed3e99d10cb6382d89aef148e1d1d1e
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-07-15 16:41:33 +0530

    updated for cpp

--- include/motor.h
+++ include/motor.h
@@ -1,9 +1,18 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void motor_init();
 void motor_set_speed(int left_pwm, int right_pwm);
 void motor_speed();
 void motor_stop();
 void motor_set_speed_ramp(int left_target, int right_target, int ramp_step, int delay_ms);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
