b8a8f290bfd9a923a10a3ee671539ffa8dbec25b
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-07-19 13:41:42 +0530

    updated header file for c++

--- include/encoder.h
+++ include/encoder.h
@@ -1,10 +1,18 @@
 #ifndef ENCODER_H
 #define ENCODER_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void encoder_init();
 void reset_ticks();
 int get_left_ticks();
 int get_right_ticks();
 int get_avg_ticks();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
