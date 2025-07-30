a023089ab5378e8913ebf19a432bfad55ac5dce0
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-07-30 21:08:19 +0530

    Initial commit

---
+++ src/power.cpp
@@ -0,0 +1,20 @@
+#include "power.h"
+#include <Adafruit_INA3221.h>
+
+Adafruit_INA3221 ina3221 = Adafruit_INA3221();
+
+bool initPowerSensor() {
+    return ina3221.begin();
+}
+
+float getBusVoltage(uint8_t channel) {
+    return ina3221.getBusVoltage(channel);
+}
+
+float getShuntVoltage(uint8_t channel) {
+    return ina3221.getShuntVoltage(channel);
+}
+
+float getCurren(uint8_t channel) {
+    return ina3221.getCurrentAmps(channel);
+}
