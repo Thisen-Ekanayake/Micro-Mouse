29fd75aacf383cd1753a678db912c19dd49bb689
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-07-30 20:34:31 +0530

    added oled display settings

--- include/config.h
+++ include/config.h
@@ -7,6 +7,7 @@
 #define ENCODER_RIGHT_A     32
 #define ENCODER_RIGHT_B     33
 
+
 // ==== Motor Control Pins (DRV8833) ====
 // Left Motor (connected to Ain1 and Ain2)
 #define LEFT_MOTOR_IN1      19  // Ain1
@@ -15,6 +16,7 @@
 // Right Motor (connected to Bin1 and Bin2)
 #define RIGHT_MOTOR_IN1     26  // Bin1 26
 #define RIGHT_MOTOR_IN2     27  // Bin2 27
+
 
 // ==== PWM ====
 #define PWM_CHANNEL_LEFT    0
@@ -32,24 +34,36 @@
 #define IR_THRESHOLD_LEFT   400
 #define IR_THRESHOLD_RIGHT  400
 
+
 // ==== IMU (MPU6050) ====
 #define MPU_SDA             21
 #define MPU_SCL             22
 
+
 // ==== Button ====
 #define BUTTON_PIN          4
+
 
 // ==== Motion Constants ====
 #define TICKS_PER_CM        10.0f   // temporary guess - adjust after testing
 #define CELL_SIZE_CM        18
 #define MAZE_SIZE           16
 
+
 // ==== Battery (if monitored) ====
 #define BATTERY_FULL_VOLTS  12.6
 #define BATTERY_LOW_VOLTS   10.5
 
+
 // ==== PID ====
 #define PID_INTEGRAL_MAX    500.0f
 #define PID_INTEGRAL_MIN   -500.0f
+
+
+// ==== OLED settings ====
+#define SCREEN_WIDTH        128
+#define SCREEN_HEIGHT       64
+#define OLED_RESET          -1
+#define SCREEN_ADDRESS      0x3C
 
 #endif
