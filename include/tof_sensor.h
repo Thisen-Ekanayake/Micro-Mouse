72bf4a4f179a4747a2210d44f7f9b6e02908c2f1
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-08-02 09:56:38 +0530

    updated for cpp

--- include/tof_sensor.h
+++ include/tof_sensor.h
@@ -3,7 +3,15 @@
 
 #include <Arduino.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void initToFSensor();
 void readToF(uint16_t &d1, uint16_t &d2, uint16_t &d3, String &l, String &r, String &f);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
