3cdcb04da469b7c2465a92556f1e856c295dfa48
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-08-03 23:54:13 +0530

    added readToFDistance() method

--- include/tof_sensor.h
+++ include/tof_sensor.h
@@ -9,6 +9,7 @@ extern "C" {
 
 void initToFSensor();
 void readToF(uint16_t &d1, uint16_t &d2, uint16_t &d3, String &l, String &r, String &f);
+void readToFDistance(uint16_t &left, uint16_t &right, uint16_t &front);
 
 #ifdef __cplusplus
 }
