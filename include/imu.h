bec6e18f42842c3a5e6ef8e57b59acdecef4ec3e
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-08-04 00:12:07 +0530

    replaced imu_reset_heading() with imu_set_heading_offset()

--- include/imu.h
+++ include/imu.h
@@ -5,6 +5,6 @@ void imu_init();             // Initialize and calibrate the IMU
 void imu_update();           // Call this regularly (e.g., every 5–10ms)
 float imu_get_heading();     // Get Z-axis angle (yaw in degrees)
 float imu_get_raw_angle();   // (Optional) for direct use if normalization added
-void imu_reset_heading();
+void imu_set_heading_offset(float offset);
 
 #endif
