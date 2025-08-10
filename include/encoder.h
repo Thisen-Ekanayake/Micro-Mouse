0b99cfd29ba59b131bfd6bfaee75dea833f96a93
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-08-11 01:59:55 +0530

    updated method types to int32_t

--- include/encoder.h
+++ include/encoder.h
@@ -1,15 +1,17 @@
 #ifndef ENCODER_H
 #define ENCODER_H
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void encoder_init();
 void reset_ticks();
-int get_left_ticks();
-int get_right_ticks();
-int get_avg_ticks();
+int32_t get_left_ticks();
+int32_t get_right_ticks();
+int32_t get_avg_ticks();
 
 #ifdef __cplusplus
 }
