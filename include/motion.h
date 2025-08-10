6ddd7e04fa39812754c66e90664b1d8d5dc1c335
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-08-11 03:11:24 +0530

    updated for c++

--- include/motion.h
+++ include/motion.h
@@ -1,10 +1,17 @@
 #ifndef MOTION_H
 #define MOTION_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // high-level movement APIs
 void move_forward_cm(float cm);
 void move_backward_cm(float cm);
 void rotate_90_left();
 void rotate_90_right();
 
+#ifdef __cplusplus
+}
+#endif
 #endif
